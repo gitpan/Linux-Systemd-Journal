886f4981607ad1c049f116385bc6edf11fa59143
Ioan Rogers <ioanr@cpan.org>
2014-05-11 20:57:19 +0000

    Import of IOANR/Linux-Systemd-Journal-0.001 from CPAN.
    gitpan-cpan-distribution: Linux-Systemd-Journal
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         IOANR/Linux-Systemd-Journal-0.001.tar.gz
    gitpan-cpan-author:       IOANR
    gitpan-cpan-maturity:     released

---
+++ inc/My/Builder.pm
@@ -0,0 +1,39 @@
+package My::Builder;
+
+use v5.10.1;
+use strict;
+use warnings;
+use base 'Module::Build';
+use lib "inc";
+
+use Try::Tiny;
+use ExtUtils::PkgConfig;
+
+sub new {
+    my ($class, %args) = @_;
+
+    my $pkg_name = 'libsystemd-journal';
+    my %pkg_info;
+
+    try {
+        %pkg_info = ExtUtils::PkgConfig->find($pkg_name);
+    }
+    catch {
+        say "Do you need to install libsystemd-journal-dev (debian) or systemd-devel (fedora)?";
+        exit;
+    };
+
+    say "Found libsystemd-journal-dev version: $pkg_info{modversion}";
+
+    if (defined $pkg_info{cflags}) {
+        $args{extra_compiler_flags} = $pkg_info{cflags};
+    }
+    $args{extra_compiler_flags} .= ' -std=c99';
+    $args{extra_linker_flags} = $pkg_info{libs};
+
+    my $builder = Module::Build->new(%args);
+
+    return $builder;
+}
+
+1;
